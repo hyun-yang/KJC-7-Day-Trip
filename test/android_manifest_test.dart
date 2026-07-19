6fc2d9e0ebc1f02596213cbea965c21f59e3d032
hyun-yang <iamyhs@gmail.com>
2026-07-19 21:01:02 +1000

    feat: add KJC Android launcher icon

--- test/android_manifest_test.dart
+++ test/android_manifest_test.dart
@@ -23,4 +23,30 @@ void main() {
       expect(manifest, contains('android:label="KJC 7-Day Trip"'));
     },
   );
+
+  test('Android launcher uses the KJC adaptive icon assets', () {
+    final pubspec = File('pubspec.yaml').readAsStringSync();
+    final manifest = File(
+      'android/app/src/main/AndroidManifest.xml',
+    ).readAsStringSync();
+    final adaptiveIcon = File(
+      'android/app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml',
+    );
+
+    expect(pubspec, contains('assets/branding/kjc_launcher_icon.png'));
+    expect(manifest, contains('android:icon="@mipmap/ic_launcher"'));
+    expect(adaptiveIcon.existsSync(), isTrue);
+    expect(
+      adaptiveIcon.readAsStringSync(),
+      contains('@drawable/ic_launcher_foreground'),
+    );
+    for (final density in ['mdpi', 'hdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi']) {
+      expect(
+        File(
+          'android/app/src/main/res/mipmap-$density/ic_launcher.png',
+        ).existsSync(),
+        isTrue,
+      );
+    }
+  });
 }
