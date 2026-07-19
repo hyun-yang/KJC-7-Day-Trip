72c244acfe8f78d0a4af99c305c1d127e1f54cd2
hyun-yang <iamyhs@gmail.com>
2026-07-19 14:57:53 +1000

    feat: add East Asia splash illustration concepts

---
+++ test/ui/east_asia_splash_assets_test.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const assets = [
+    'assets/illustrations/atlas_postcard_splash.svg',
+    'assets/illustrations/cultural_seals_splash.svg',
+    'assets/illustrations/journey_line_splash.svg',
+  ];
+
+  test('ships three text-free East Asia splash SVG concepts', () {
+    for (final asset in assets) {
+      final source = File(asset).readAsStringSync();
+      expect(source, contains('<svg'));
+      expect(source, contains('viewBox='));
+      expect(source, isNot(contains('<text')));
+    }
+  });
+}
